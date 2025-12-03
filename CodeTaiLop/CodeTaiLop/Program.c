7e2788bb180466e4d37c6a6cebe0e1b9b71f0f43
vinhpqph68712 <98261160+toanp5287@users.noreply.github.com>
2025-12-03 10:48:28 +0700

    Add project files.

---
+++ CodeTaiLop/CodeTaiLop/Program.c
@@ -0,0 +1,12 @@
+// CConsoleApplication.c : file nay chua 'main' function. 
+// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi there!\n");
+}
+
+
+// GV: AnhTT184
