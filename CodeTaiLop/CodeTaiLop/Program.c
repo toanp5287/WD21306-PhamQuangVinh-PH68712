8496dd53a8b39cb129605a91ed2f337d382caa5c
vinhpqph68712 <toanp5287@gmail.com>
2025-12-10 11:23:52 +0700

    bai 10/12

--- CodeTaiLop/CodeTaiLop/Program.c
+++ CodeTaiLop/CodeTaiLop/Program.c
@@ -3,10 +3,93 @@
 
 #include <stdio.h>
 
+void thongTinMonHoc()
+{
+	char tenMonHoc[50];
+	int tinChi;
+
+	while (getchar() != '\n');
+	printf("nhap ten mon hoc: ");
+	fgets(tenMonHoc, sizeof(tenMonHoc), stdin);
+
+	printf("nhap so tien tin chi: ");
+	scanf("%d", &tinChi);
+
+	printf("mon hoc:%s", tenMonHoc);
+	puts(tenMonHoc);
+	
+	printf("tien tin chi: %d\n", tinChi);
+}
+
+void tinhTongSoLe(int n)
+{
+	int tong = 0;
+	for (int i = 0; i < n; i++)
+	{
+		tong += i;
+		printf("tong la %d\n", tong);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0) {
+			printf("%d la so chan\n", i);
+		}
+		else
+		{
+			printf("%d la so le\n", i);
+		}
+	}
+}
+void thongTinDiemLab()
+{
+
+ }
 int main()
 {
-    printf("Hi there!\n");
+	int chon, tiepTuc;
+	int x;
+	do {
+		printf("MENU\n");
+		printf("1. thong tin mon hoc\n");
+		printf("2 tinh tong so le\n");
+		printf("3 thong ton diem lab\n");
+		printf("0 thoat\n");
+
+		printf("moi chon chuc nang: ");
+		scanf("%d", &chon);
+
+		if (chon == 0)
+		{
+			printf("thoat\n");
+			return;
+		}
+		tiepTuc = 1;
+		while (tiepTuc == 1)
+		{
+			switch (chon)
+			{
+			case 1:
+				thongTinMonHoc();
+				break;
+			case 2:
+				printf("nhap so nguyen duong x: ");
+				scanf("%d", &x);
+				tinhTongSoLe(x);
+				break;
+			case 3:
+				thongTinDiemLab();
+				break;
+			default:
+				printf("chon lai [0-3]: ");
+				break;
+			}
+			printf("ban muon tiep tuc menu: ");
+			scanf("%d", &tiepTuc);
+			syteam("cls");
+		}
+	} while (chon != 0);
 }
+
 
 
 // GV: AnhTT184
--- Lab/Lab/Program.c
+++ Lab/Lab/Program.c
@@ -3,30 +3,93 @@
 
 #include <stdio.h>
 
-int main()
+void thongTinMonHoc()
 {
-    printf("wecom to school\n");
-    return 0;
+	char tenMonHoc[50];
+	int tinChi;
+
+	while (getchar() != '\n');
+	printf("nhap ten mon hoc: ");
+	fgets(tenMonHoc, sizeof(tenMonHoc), stdin);
+
+	printf("nhap so tien tin chi: ");
+	scanf("%d", &tinChi);
+
+	printf("mon hoc:%s", tenMonHoc);
+	puts(tenMonHoc);
+
+	printf("tien tin chi: %d\n", tinChi);
 }
 
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
+void tinhTongSoLe(int n)
+{
+	int tong = 0;
+	for (int i = 0; i < n; i++)
+	{
+		tong += i;
+		printf("tong la %d\n", tong);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0) {
+			printf("%d la so chan\n", i);
+		}
+		else
+		{
+			printf("%d la so le\n", i);
+		}
+	}
+}
+void thongTinDiemLab()
+{
 
-// Danh cho nguoi moi bat dau:
-//   1. Mo "View" > "Solution Explorer"
-//          de them/quan ly cac files
-//   2. Mo "View" > "Output"
-//          de kiem tra "build output" va "cac thong bao khac"
-//   3. Mo "View" > "Error List"
-//          de xem loi
-//   4. Chon Project > "Add" > "New Item"
-//          de tao moi file code,
-//      hoac Project > "Add" > "Existing Item"
-//          de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//          chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//          trong cua so "Solution Explorer"
-//          bam chuot phai vao "Solution"
-//          chon "Open Folder in File Explorer"
+}
+int main()
+{
+	int chon, tiepTuc;
+	int x;
+	do {
+		printf("MENU\n");
+		printf("1. thong tin mon hoc\n");
+		printf("2 tinh tong so le\n");
+		printf("3 thong ton diem lab\n");
+		printf("0 thoat\n");
+
+		printf("moi chon chuc nang: ");
+		scanf("%d", &chon);
+
+		if (chon == 0)
+		{
+			printf("thoat\n");
+			return;
+		}
+		tiepTuc = 1;
+		while (tiepTuc == 1)
+		{
+			switch (chon)
+			{
+			case 1:
+				thongTinMonHoc();
+				break;
+			case 2:
+				printf("nhap so nguyen duong x: ");
+				scanf("%d", &x);
+				tinhTongSoLe(x);
+				break;
+			case 3:
+				thongTinDiemLab();
+				break;
+			default:
+				printf("chon lai [0-3]: ");
+				break;
+			}
+			printf("ban muon tiep tuc menu: ");
+			scanf("%d", &tiepTuc);
+			syteam("cls");
+		}
+	} while (chon != 0);
+}
+
+
 
 // GV: AnhTT184
